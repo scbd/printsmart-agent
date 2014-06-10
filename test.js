9548030d19061bd1669d606cc1ca3d25035c8f4b
Philippe Leblond <leblond.philippe@gmail.com>
2014-06-10 14:26:15 -0400

    enable testing

---
+++ test.js
@@ -0,0 +1,15 @@
+var when = require('when');
+var agent = require('./agent');
+
+var message = {
+    id: '5395D9-E9915C46C8-72B4C9E2',
+    url: 'http://www.cbd.int/doc/meetings/abs/absws-2014-06/other/absws-2014-06-info-note-en.pdf',
+    symbol: 'UNEP/CBD/ABS/WS/2014/6/1',
+    language: 'EN',
+    name: 'John Smith - France',
+    box: '0021'
+}
+
+when(agent.processMessage(message), function() {
+    console.log('DONE');
+});
