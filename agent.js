174cfcfaa111438bb945aa32e46cc8567f3f363c
Frederic Vogel <fvogel@CUPS.(none)>
2014-06-10 12:56:05 -0400

    add the raw option to lp cups does not strip in out

--- agent.js
+++ agent.js
@@ -176,7 +176,7 @@ function print(filepath) {
 
     var title = message.box + ': ' + message.symbol + ' (' + message.language + ') - ' + message.name;
 
-    var ls = processx.spawn('lp', [filepath, '-t', title]);
+    var ls = processx.spawn('lp', ['-o raw', filepath, '-t', title]);
 
     ls.stdout.on('data', function (data) { console.log('stdout: ' + data); });
     ls.stderr.on('data', function (data) { console.log('stderr: ' + data); });
