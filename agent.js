068859163c7dcd5ff5c87d0b616216d8b74ed5e0
Stephane Bilodeau <bilodeaustephane@gmail.com>
2014-09-28 14:30:12 +0900

    Allow multi jobs for stupid pcl printers

--- agent.js
+++ agent.js
@@ -20,6 +20,8 @@ function AgentClass() {
         apiVersion: '2012-11-05',
     });
 
+    var nextPrintShopPrinter = 0;
+
     //============================================================
     //
     //
@@ -28,7 +30,16 @@ function AgentClass() {
     {
         var box = normalize(message.box);
 
-        if(message.anonymous) return 'ipp://localhost:631/classes/printshop';
+        if(message.anonymous) { // For printshop
+
+            nextPrintShopPrinter++;
+
+            if(nextPrintShopPrinter%5===1) { message.multijobs=true; return 'ipp://localhost:631/classes/printshop-pcl-12'; }
+            if(nextPrintShopPrinter%5===3) { message.multijobs=true; return 'ipp://localhost:631/classes/printshop-pcl-14'; }
+            if(nextPrintShopPrinter%5===4) { message.multijobs=true; return 'ipp://localhost:631/classes/printshop-pcl-15'; }
+
+            return 'ipp://localhost:631/classes/printshop';
+        }
 
         if(box<'0060') return 'ipp://localhost:631/classes/ctr-a';
         if(box<'0130') return 'ipp://localhost:631/classes/ctr-b';
@@ -47,9 +58,9 @@ function AgentClass() {
     //============================================================
     this.processMessage = function processMessage(message) {
 
-        console.log('Processing:', message);
-
         message.printerUri = selectQueue(message);
+
+        console.log('Processing:', message);
 
         var filenames = [ nodefn.call(tmp.tmpName, { postfix: '.pdf'       } ),
                           nodefn.call(tmp.tmpName, { postfix: '.ps'        } ),
@@ -278,7 +289,8 @@ function print(filename, message) {
 
     var printer = ipp.Printer(message.printerUri);
 
-    var title = message.box + ': ' + message.symbol + ' (' + message.language + ') - ' + message.name;
+    var title  = message.box + ': ' + message.symbol + ' (' + message.language + ') - ' + message.name;
+    var copies = Math.round(message.copies || 1);
 
     var options = {
         'operation-attributes-tag': {
@@ -289,10 +301,19 @@ function print(filename, message) {
         "job-attributes-tag": {
             'sides': 'two-sided-long-edge',
             'finishings': ['staple'],
-            'copies' : Math.round(message.copies || 1).toString()
+            'copies' : copies
         },
         data: fs.readFileSync(filename)
     };
+
+    if(message.multijobs && copies>1) { //Send copies-1 copy of document for stupid PCL canon printers
+
+        delete options['job-attributes-tag'].copies;
+
+        for(var i=1;i<copies;++i) {
+            nodefn.call(printer.execute.bind(printer), "Print-Job", options);
+        }
+    }
 
     return when(nodefn.call(printer.execute.bind(printer), "Print-Job", options), function (res) {
         return res['job-attributes-tag']['job-uri'];
