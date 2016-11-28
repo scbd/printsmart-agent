bd8e21b6e8e21a511b004300c4c387d32937d6e8
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-11-29 00:44:59 +0100

    Only use evironment vars for instanceID

--- config.js
+++ config.js
@@ -1,18 +1,14 @@
-var configPath = process.env.CONFIG_FILE || './config.json';
+console.log(process.env.INSTANCE_ID);
+console.log(process.env.CONFIG_FILE);
 
-console.log("CONFIG PATH", configPath);
+if(!process.env.CONFIG_FILE) throw new Error("Environment variable CONFIG_FILE not set");
+if(!process.env.INSTANCE_ID) throw new Error("Environment variable INSTANCE_ID not set");
 
-var config = require(configPath);
-config.instanceId             = instanceId();
+var config = require(process.env.CONFIG_FILE);
+
+config.instanceId             = process.env.INSTANCE_ID.replace(/[^a-z0-9]/i, '_');
 config.queues.print           = config.queues.print          .replace('{instanceId}', config.instanceId);
 config.queues.reportJobStatus = config.queues.reportJobStatus.replace('{instanceId}', config.instanceId);
 config.queues.updateJobStatus = config.queues.updateJobStatus.replace('{instanceId}', config.instanceId);
 
 module.exports = exports = config;
-
-//==============================
-//
-//==============================
-function instanceId(){
-    return require('os').hostname().replace(/[^a-z0-9]/i, '_');
-}
